de0f888996d30094881ffa27957d9d5c61dcbac3
SrdjanLazic <slazic3219rn@raf.rs>
2022-01-03 20:19:44 +0100

    Initial commit

---
+++ src/main/java/com/raf/hotelreservationservice/HotelReservationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.raf.hotelreservationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelReservationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelReservationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raf/hotelreservationservice/HotelReservationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raf.hotelreservationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelReservationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
